9a2f0eda2856dbe8e9f68905bbfee1df9fe5be49
GabrielaMoralesR <up240055@alumnos.upa.edu.mx>
2026-03-01 10:38:39 -0600

    Tarea de Types

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,58 @@
 module Helper exposing (..)
+
+-- Calificaciones
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+gradetoCategory : Float -> GradeStatus
+gradetoCategory grade =
+    if grade > 7.0 then
+        Approved
+
+    else if grade < 0.0 then
+        Pending
+
+    else
+        Failed
+
+
+categoricalGrade : (Float -> GradeStatus) -> List Float -> List GradeStatus
+categoricalGrade gradetoCategory1 grades =
+    List.map gradetoCategory1 grades
+
+
+
+--Aeropuerto
+
+
+type AireplaneStatus
+    = OnTime
+    | Boarding
+    | Delayed
+    | Cancelled
+
+
+airplaneScheduleAction : AireplaneStatus -> String
+airplaneScheduleAction accion =
+    case accion of
+        OnTime ->
+            "Pedir reembolso"
+
+        Boarding ->
+            "Esperar"
+
+        Delayed ->
+            "Esperar"
+
+        Cancelled ->
+            "Buscar boleto"
+
+
+airportAction : (AireplaneStatus -> String) -> List AireplaneStatus -> List String
+airportAction accion estado =
+    List.map accion estado
